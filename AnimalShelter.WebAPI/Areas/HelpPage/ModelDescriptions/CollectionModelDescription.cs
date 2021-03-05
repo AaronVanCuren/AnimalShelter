39d125f157ccb2d93542b5fdb06d27d54e05017b
Aaron Van Curen <aaronvancuren@gmail.com>
2021-03-05 09:12:52 -0500

    Add project files.

---
+++ AnimalShelter.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AnimalShelter.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ AnimalShelter.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AnimalShelter.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
