3ac9b70cd65611c8c4c72a3300e7da62162a6c79
dbenge89 <davidcbenge89@gmail.com>
2021-03-05 10:14:48 -0500

    AnimalClass

--- AnimalShelter.Data/Animal.cs
+++ AnimalShelter.Data/Animal.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AnimalShelter.Data
 {
-    class Animal
+    public enum SpeciesType { Cat, Dog, Bunny}
+    public class Animal
     {
+        [Key]
+        [Required]
+        public int AnimalId { get; set; }
+        public string Name { get; set; }
+        [Required]
+        public SpeciesType Species { get; set; }
+        public string Breed { get; set; }
+        [Required]
+        public bool Sex { get; set; }
+        [Required]
+        public bool Fixed { get; set; }
+        public bool HasShots { get; set; }
+        [Required]
+        public string Age { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public decimal AdoptionPrice { get; set; }
     }
 }
