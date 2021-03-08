2abce6a5fa3679c8498eb474418de52c0135e652
dbenge89 <davidcbenge89@gmail.com>
2021-03-08 10:06:13 -0500

    Added properties to animalpost class renamed post

---
+++ AnimalShelter.Data/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelter.Data
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        [Required]
+        public int AnimalId { get; set; }
+        [Required]
+        public int CompanyId { get; set; }
+    }
+}
