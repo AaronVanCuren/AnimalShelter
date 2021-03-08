2fbeefad1e293d03277464f4c94531564edafb88
dbenge89 <davidcbenge89@gmail.com>
2021-03-08 10:19:44 -0500

    updated post class... again. Love GitHub

--- AnimalShelter.Data/Post.cs
+++ AnimalShelter.Data/Post.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,11 @@ namespace AnimalShelter.Data
         public int PostId { get; set; }
         [Required]
         public int AnimalId { get; set; }
+        [ForeignKey(nameof(AnimalId))]
+        public virtual Animal Animal { get; set; }
         [Required]
         public int CompanyId { get; set; }
+        [ForeignKey(nameof(CompanyId))]
+        public virtual Company Company { get; set; }
     }
 }
