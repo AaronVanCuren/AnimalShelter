09c64136eaa7b13c52732132be535630f584a82d
Aaron Van Curen <aaronvancuren@gmail.com>
2021-03-15 09:45:23 -0400

    Updated Profile Usage

--- AnimalShelter.Data/IdentityModels.cs
+++ AnimalShelter.Data/IdentityModels.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
@@ -9,9 +11,42 @@ using Microsoft.AspNet.Identity.Owin;
 
 namespace AnimalShelter.Data
 {
+    public enum UserType { customer, company, vet}
+
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public Guid UserId { get; set; }
+
+        public UserType UserType { get; set; }
+
+        public string FullName { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string Address { get; set; }
+        
+        public virtual List<Vaccine> Vaccines { get; set; }
+
+        public virtual List<Animal> Animals { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+
+        public virtual List<CompanyRating> Ratings { get; set; }
+
+        public double Rating
+        {
+            get
+            {
+                double TotalAverageRating = 0;
+                foreach (var rating in Ratings)
+                {
+                    TotalAverageRating += rating.AverageScore;
+                }
+                return (Ratings.Count > 0) ? TotalAverageRating / Ratings.Count : 0;
+            }
+        }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -33,15 +68,13 @@ namespace AnimalShelter.Data
             return new ApplicationDbContext();
         }
 
+        public DbSet<Adoption> Adoptions { get; set; } 
+        public DbSet<Animal> Animals { get; set; }
         public DbSet<CompanyRating> Ratings { get; set; }
         public DbSet<Vaccine> Vaccines { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Reply> Replies { get; set; }
-        public DbSet<Animal> Animals { get; set; }
-        public DbSet<Profile> Profiles { get; set; }
-        public DbSet<Company> Companies { get; set; }
-        public DbSet<Adoption> Adoptions { get; set; } 
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
---
+++ AnimalShelter.Services/UserService.cs
@@ -0,0 +1,108 @@
+﻿using AnimalShelter.Data;
+using AnimalShelter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelter.Services
+{
+    public class UserService
+    {
+        public IEnumerable<ApplicationUser> GetCustomers()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var query = db.Users.Where(e => e.UserType == UserType.customer)
+                    .Select(e => new ApplicationUser
+                    {
+                        UserName = e.UserName,
+                        FullName = e.FullName,
+                        Address = e.Address
+                    });
+
+                return query.ToArray();
+            }
+        }
+
+        public IEnumerable<ApplicationUser> GetCompanies()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var query = db.Users.Where(e => e.UserType == UserType.company)
+                    .Select(e => new ApplicationUser
+                    {
+                        UserName = e.UserName,
+                        CompanyName = e.CompanyName,
+                        Address = e.Address,
+                        Posts = e.Posts,
+                        Ratings = e.Ratings
+                    });
+
+                return query.ToArray();
+            }
+        }
+
+        public IEnumerable<ApplicationUser> GetVets()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var query = db.Users.Where(e => e.UserType == UserType.vet)
+                    .Select(e => new ApplicationUser
+                    {
+                        UserName = e.UserName,
+                        FullName = e.FullName,
+                        Address = e.Address,
+                        Vaccines = e.Vaccines
+                    });
+
+                return query.ToArray();
+            }
+        }
+
+        public ProfileRUD GetProfileById(int id)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var entity = db.Profiles
+                        .Single(e => e.ProfileId == id && e.UserId == _userId);
+                return new ProfileRUD
+                {
+                    ProfileId = entity.ProfileId,
+                    FirstName = entity.FirstName,
+                    LastName = entity.LastName,
+                    Address = entity.Address
+                };
+            }
+        }
+
+        public bool UpdateProfile(ProfileRUD model)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var entity = db.Profiles
+                        .Single(e => e.ProfileId == model.ProfileId && e.UserId == _userId);
+
+                entity.FirstName = model.FirstName;
+                entity.LastName = model.LastName;
+                entity.Address = model.Address;
+
+                return db.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteProfile(int profileId)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var entity = db.Profiles
+                        .Single(e => e.ProfileId == profileId && e.UserId == _userId);
+
+                db.Profiles.Remove(entity);
+
+                return db.SaveChanges() == 1;
+            }
+        }
+    }
+}
